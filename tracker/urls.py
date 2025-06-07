d7b9dcd759e8d677f3327a7559e2d57cfbfbd3f6
Kirill <kirill2014.lambin.11@gmail.com>
2025-06-07 22:53:28 +0700

    Добавлены страницы для предметов, оценок и отчёта с берёзово-белым стилем

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,4 +5,7 @@ app_name = 'tracker'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('subjects/', views.subject_list, name='subject_list'),
+    path('grades/', views.grade_list, name='grade_list'),
+    path('report/', views.report, name='report'),
     ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student, Subject, Grade
+from django.db.models import Avg
 
 def index(request):
     students = Student.objects.all()
     return render(request, 'tracker/index.html', {'students': students})
+
+def subject_list(request):
+    subjects = Subject.objects.all()
+    return render(request, 'tracker/subject_list.html', {'subjects': subjects})
+
+def grade_list(request):
+    grades = Grade.objects.all()
+    return render(request, 'tracker/grade_list.html', {'grades': grades})
+
+def report(request):
+    students = Student.objects.all()
+    report_data = []
+    for student in students:
+        avg_score = Grade.objects.filter(student=student).aggregate(Avg('score'))['score__avg']
+        report_data.append({
+            'student': student,
+            'avg_score': round(avg_score, 2) if avg_score else 0,
+            })
+        return render(request, 'tracker/report.html', {'report_data': report_data})
